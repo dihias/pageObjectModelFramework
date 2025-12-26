feb587a62c47e92f443a416776b4ed28d4a18c97
dihia <dihiasalhi@gmail.com>
2025-12-26 15:49:23 -0500

    cars names and prices

--- Pages/CarBase.py
+++ Pages/CarBase.py
@@ -13,3 +13,14 @@ class CarBase:
         return WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, xpath))
         ).text
+
+    def getCarNameAndPrice(self):
+        names_XPATH = configReader.readConfig("locators", "carName_XPATH")
+        prices_XPATH = configReader.readConfig("locators", "carPrice_XPATH")
+
+        carNames=self.driver.find_elements(By.XPATH, names_XPATH)
+        carPrices=self.driver.find_elements(By.XPATH, prices_XPATH)
+
+        for i in range(1,len(carPrices)):
+            print(carNames[i].text + "--------prices are : "+ carPrices[i].text)
+
--- TestCases/test_carewale.py
+++ TestCases/test_carewale.py
@@ -25,7 +25,7 @@ class Test_Carwale(BaseTest):
         home.go_to_new_cars()
         time.sleep(3)
 
-    #@pytest.mark.skip
+    @pytest.mark.skip
     @pytest.mark.parametrize("carBrand,carTitle", dataProvider.get_data("NewCarsTest"))
     def test_selectCars(self, carBrand,carTitle):
         log.logger.info("******Inside Select Cars Test*********")
@@ -53,3 +53,35 @@ class Test_Carwale(BaseTest):
            title = car.getCarTitle()
            print("Car Title is : " + title)
            assert title == carTitle, "Not on the correct page as title is not matching"
+
+    @pytest.mark.parametrize("carBrand,carTitle", dataProvider.get_data("NewCarsTest"))
+    def test_printCarsNamesAndPrices(self, carBrand, carTitle):
+        log.logger.info("******Inside Cars Names and prices Test*********")
+        home = HomePage(self.driver)
+        car = CarBase(self.driver)
+
+        print("Car brand is : ", carBrand)
+        if carBrand == "BMW":
+            home.go_to_new_cars().selectBMW()
+            title = car.getCarTitle()
+            print("Car Title is : " + title)
+            assert title == carTitle, "Not on the correct page as title is not matching"
+            car.getCarNameAndPrice()
+        elif carBrand == "Hyundai":
+            home.go_to_new_cars().selectHyundai()
+            title = car.getCarTitle()
+            print("Car Title is : " + title)
+            assert title == carTitle, "Not on the correct page as title is not matching"
+            car.getCarNameAndPrice()
+        elif carBrand == "Honda":
+            home.go_to_new_cars().selectHonda()
+            title = car.getCarTitle()
+            print("Car Title is : " + title)
+            assert title == carTitle, "Not on the correct page as title is not matching"
+            car.getCarNameAndPrice()
+        elif carBrand == "Toyota":
+            home.go_to_new_cars().selectToyota()
+            title = car.getCarTitle()
+            print("Car Title is : " + title)
+            assert title == carTitle, "Not on the correct page as title is not matching"
+            car.getCarNameAndPrice()
